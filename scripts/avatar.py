bfab679e3629b168138ea6f8d9902715a763a5cb
IT-corridor <jason.5001001@gmail.com>
2019-09-12 14:49:59 +0530

    feat: save image local

--- scripts/avatar.py
+++ scripts/avatar.py
@@ -1,11 +1,15 @@
-import requests
 import os
+import requests
 from os import sys, path
+
 import django
+from PIL import Image
 
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fantasy_nfl.settings")
 django.setup()
+
+from django.conf import settings
 
 from general.models import *
 from general import html2text
@@ -39,7 +43,31 @@ def get_avatars(teams):
             for ii in _players:
                 first_name = sync("name", ii['firstName']).replace('.', '')
                 last_name = sync("name", ii['lastName']).replace('.', '')
-                Player.objects.filter(team=team, first_name=first_name, last_name=last_name).update(eid=ii['id'])
+
+                eid = ii['id']
+                img_path = path.join(settings.BASE_DIR, "static/media")
+                normal_file = img_path + "/normal/{}.png".format(eid)
+                icon_file = img_path + "/icon/{}.png".format(eid)
+                img_url = 'http://a.espncdn.com/combiner/i?img=/i/headshots/nfl/players/full/{}.png'.format(eid)
+
+                if not path.exists(normal_file):
+                    try:
+                        # download image and save as normal file
+                        img_content = requests.get(img_url).content
+                        with open(normal_file, 'wb') as f:
+                            f.write(img_content)
+
+                        # resize and saev as icon file
+                        height = 24
+                        img = Image.open(normal_file)
+                        hpercent = (height / float(img.size[1]))
+                        wsize = int((float(img.size[0]) * float(hpercent)))
+                        img = img.resize((wsize, height), Image.ANTIALIAS)
+                        img.save(icon_file) 
+                    except Exception as e:
+                        eid = -1
+
+                Player.objects.filter(team=team, first_name=first_name, last_name=last_name).update(eid=eid)
         # except:
         #     print('*** Something is wrong ***')
 
